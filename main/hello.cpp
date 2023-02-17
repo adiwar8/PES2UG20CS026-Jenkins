05094d203bc03410504000eda98fb5b5235416df
adiwar8 <79269003+adiwar8@users.noreply.github.com>
2023-02-18 00:29:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<< "PES2UG20CS026\n";
-  cout<< "TASK4 DONE";
   cout << "I have successfully built and run\n";
   return 0;
 }
